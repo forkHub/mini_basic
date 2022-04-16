a1c91b20db5e5d51729df1de9e7a4228a8ee6a6e
frokhman <fajar.rokhman@verint.com>
2022-04-16 17:35:42 +0700

    a

---
+++ lib/src/comp/Logger.ts
@@ -0,0 +1,37 @@
+namespace ha.comp {
+    class Logger2 {
+        private _aktif: boolean = false;
+        public get aktif(): boolean {
+            return this._aktif;
+        }
+        public set aktif(value: boolean) {
+            this._aktif = value;
+        }
+
+        constructor() {
+
+        }
+
+        group(msg: any): void {
+            if (this._aktif) {
+                console.group(msg);
+                msg;
+            }
+        }
+
+        groupEnd(): void {
+            if (this._aktif) {
+                console.groupEnd();
+            }
+        }
+
+        log(msg: any): void {
+            if (this._aktif) {
+                console.log(msg);
+                msg;
+            }
+        }
+    }
+
+    export var log: Logger2 = new Logger2();
+}
